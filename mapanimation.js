8d8ce3a295955983c0c9ebd08843ccc8548199ed
Ed Waggoner <edwaggoner@gmail.com>
2022-05-10 21:05:58 -0700

    works; needs centering

--- mapanimation.js
+++ mapanimation.js
@@ -1,48 +1,36 @@
+// pass access token to mapbox.com
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2xpdmVyMTAiLCJhIjoiY2wydzhreWY2MGM3azNxbGZtczVzMWMwciJ9.dnemmICAVgbnQZLPOkoBMw';
+
+// acquire map
+let map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11',
+center: [-71.104081, 42.365554],
+zoom: 14,
+});
+
 async function run(){
-    // get real-time bus data
+	// get real-time bus data
 	const locations = await getBusLocations();
-	console.log(new Date());
-	console.log(locations);
+
+	// place marker at current location of bus
+	var marker = new mapboxgl.Marker()
+		.setLngLat([locations[0].attributes.longitude, locations[0].attributes.latitude])
+		.addTo(map);
 
 	// timer
 	setTimeout(run, 15000);
 }
 
+
+// assign different color markers to each of the four buses
+
+
 // Request real-time bus data from MBTA
 async function getBusLocations(){
 	const url = 'https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip';
+	// const url = 'https://api-v3.mbta.com/vehicles?filter[vehicles]';
 	const response = await fetch(url);
 	const json     = await response.json();
 	return json.data;
 }
-
-run ();
-
-// // give mapbox access token
-// mapboxgl.accessToken = 'pk.eyJ1Ijoic2xpdmVyMTAiLCJhIjoiY2wydzhreWY2MGM3azNxbGZtczVzMWMwciJ9.dnemmICAVgbnQZLPOkoBMw';
-
-// // acquire Boston map from mapbox
-// let map = new mapboxgl.Map({
-// container: 'map',
-// style: 'mapbox://styles/mapbox/streets-v11',
-// center: [-71.104081, 42.365554],
-// zoom: 14,
-// });
-
-// // TODO: add a marker to the map at the first coordinates in the array busStops. The marker variable should be named "marker"
-// var marker = new mapboxgl.Marker()
-// 	.setLngLat([-71.093729, 42.359244])
-// 	.addTo(map);
-// // counter here represents the index of the current bus stop
-// let counter = 0;
-// function move() {
-// // TODO: move the marker on the map every 1000ms. Use the function marker.setLngLat() to update the marker coordinates
-// // Use counter to access bus stops in the array busStops
-// // Make sure you call move() after you increment the counter.
-// setTimeout(() =>{
-// 	if (counter >= busStops.length) return;
-// 	marker.setLngLat(busStops[counter]);
-// 	counter++;
-// 	move();
-// }, 1000);
-// }
