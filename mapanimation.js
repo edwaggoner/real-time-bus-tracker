03bb6c87e6188cf9ee2f834cf1e8addca68d8370
Ed Waggoner <edwaggoner@gmail.com>
2022-05-11 17:55:23 -0700

    works! fix button

--- mapanimation.js
+++ mapanimation.js
@@ -1,36 +1,44 @@
-// pass access token to mapbox.com
+//pass access token to mapbox.com
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2xpdmVyMTAiLCJhIjoiY2wydzhreWY2MGM3azNxbGZtczVzMWMwciJ9.dnemmICAVgbnQZLPOkoBMw';
 
-// acquire map
-let map = new mapboxgl.Map({
-container: 'map',
-style: 'mapbox://styles/mapbox/streets-v11',
-center: [-71.104081, 42.365554],
-zoom: 14,
-});
+// declare map variable outside of all functions, so it can be used inside any/all functions
+let mapobj;
+
+async function centerTheMap() {
+	// obtain current location of bus
+	let initiallocation = await getBusLocations();
+
+	// display Boston map centered on that location
+	mapobj = new mapboxgl.Map({
+	container: 'map',
+	style: 'mapbox://styles/mapbox/streets-v11',
+	center: [initiallocation[0].attributes.longitude, initiallocation[0].attributes.latitude],
+	zoom: 15,
+	});
+}
+
+let centerTheMapPromise = centerTheMap();
 
 async function run(){
+	// ensure that Boston map is properly centered BEFORE the bus marker is plotted (by button-click)
+	await centerTheMapPromise;
+
 	// get real-time bus data
 	const locations = await getBusLocations();
 
 	// place marker at current location of bus
 	var marker = new mapboxgl.Marker()
 		.setLngLat([locations[0].attributes.longitude, locations[0].attributes.latitude])
-		.addTo(map);
+		.addTo(mapobj);
 
-	// timer
+	// timer to refresh data
 	setTimeout(run, 15000);
 }
-
-
-// assign different color markers to each of the four buses
-
 
 // Request real-time bus data from MBTA
 async function getBusLocations(){
 	const url = 'https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip';
-	// const url = 'https://api-v3.mbta.com/vehicles?filter[vehicles]';
 	const response = await fetch(url);
-	const json     = await response.json();
+	const json = await response.json();
 	return json.data;
 }
